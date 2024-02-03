7413e7f9f23026ef3bd14dfc14112c7a7eda5376
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-03 11:52:14 +0630

    table added

--- init.sql
+++ init.sql
@@ -15,3 +15,38 @@ CREATE TABLE books (
     image text,
     FOREIGN KEY (author_id) REFERENCES authors(id)
 );
+
+CREATE TABLE readers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    dateOfBirth DATE,
+    email VARCHAR(255) null,
+    address VARCHAR(255) null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE borrowings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    readerId INT,
+    bookId INT,
+    borrowDate DATE,
+    returnDate DATE,
+    FOREIGN KEY (readerId) REFERENCES readers(id),
+    FOREIGN KEY (bookId) REFERENCES books(id)
+);
+
+CREATE TABLE genres (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    genreName VARCHAR(255) NOT NULL,
+    createdAt timestamp default CURRENT_TIMESTAMP,
+	updatedAt timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE book_genres (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    genre_id INT,
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
