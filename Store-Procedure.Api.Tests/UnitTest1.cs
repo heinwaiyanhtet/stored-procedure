f27a2d9531ebef675914abb8ceea41a9262eda18
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-28 03:11:13 +0630

    my first unit test

--- Store-Procedure.Api.Tests/UnitTest1.cs
+++ Store-Procedure.Api.Tests/UnitTest1.cs
@@ -1,10 +1,33 @@
 namespace unit_test;
 
+public static class StringExtensions
+{
+    public static bool StartsWithUpper(this string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return false;
+        }
+
+        char firstChar = str[0];
+        return char.IsUpper(firstChar);
+    }
+}
+
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestStartsWithUpper()
     {
+        // Tests that we expect to return true.
+        string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+        foreach (var word in words)
+        {
+            bool result = word.StartsWithUpper();
 
+            Assert.True(result,
+                string.Format("Expected for '{0}': true; Actual: {1}",
+                                word, result));
+        }
     }
 }
