5a8ddc9eb965b445683741fa02a4fccc4f07d207
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-29 23:58:25 +0630

    test

--- Store-Procedure.Api.Tests/UnitTest1.cs
+++ Store-Procedure.Api.Tests/UnitTest1.cs
@@ -1,18 +1,6 @@
 namespace unit_test;
 
-public static class StringExtensions
-{
-    public static bool StartsWithUpper(this string str)
-    {
-        if (string.IsNullOrEmpty(str))
-        {
-            return false;
-        }
 
-        char firstChar = str[0];
-        return char.IsUpper(firstChar);
-    }
-}
 
 public class UnitTest1
 {
@@ -20,14 +8,22 @@ public class UnitTest1
     public void TestStartsWithUpper()
     {
         // Tests that we expect to return true.
-        string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
-        foreach (var word in words)
-        {
-            bool result = word.StartsWithUpper();
+        var processor = new TicketBookingRequestProcessor();
 
-            Assert.True(result,
-                string.Format("Expected for '{0}': true; Actual: {1}",
-                                word, result));
-        }
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Abdul",
+                LastName = "Rahman",
+                Email = "abdulrahman@demo.com"
+            };
+
+            // Act
+            TicketBookingResponse response = processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
     }
 }
